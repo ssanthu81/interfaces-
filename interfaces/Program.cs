0fcb894d12e5ca925010a061185b186af2070603
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:03:30 -0500

    Add files via upload

---
+++ interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApplication
+{
+   
+    interface IAnimal
+    {
+        void animalSound(); 
+    }
+
+
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();  
+            myPig.animalSound();
+        }
+    }
+}
+
